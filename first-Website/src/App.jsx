99bef35b08a00b4e2fb16dcc89768451f313b060
Ranjan Yadav <ranjanyadav5405@gmail.com>
2024-08-30 17:08:38 +0530

    card

--- first-Website/src/App.jsx
+++ first-Website/src/App.jsx
@@ -1,3 +1,13 @@
 export const App = () =>{
-  return <h1> Ranjan Kumar Yadav</h1>
+  return(
+    <div>
+      <img src="abc.jpg" alt="abc.jpg" width= "40%" height="40%" />
+     <h2>Queen of Latina</h2>
+     <h3>Rating: 8.4</h3>
+     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam hic quae saepe neque, distinctio obcaecati repudiandae esse excepturi, 
+          reiciendis, quo commodi ipsa dignissimos.</p>  
+    </div>
+    
+    )
+  
 }
