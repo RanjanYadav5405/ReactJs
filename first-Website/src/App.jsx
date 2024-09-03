ce6ac249cce53004d8bbb989e77a0534b8449771
Ranjan Yadav <ranjanyadav5405@gmail.com>
2024-09-03 16:10:38 +0530

    cards

--- first-Website/src/App.jsx
+++ first-Website/src/App.jsx
@@ -1,12 +1,14 @@
 // import NetflixMovies, {Header, Footer } from './components/NetflixMovies'
 import NetflixSeries from './components/NetflixSeries'
-import "./components/Netflix.css"
+// import "./components/Netflix.css"
+
+import styles from "./components/Netflix.module.css"
 
 
 export const App = () =>{
    return( 
     <section className='container'>
-      <h1 className='card-heading'>
+      <h1 className={styles['card-heading']}>
         List of best Netflix Series
       </h1>
     {/* <Header/>
--- first-Website/src/components/Lists.jsx
+++ first-Website/src/components/Lists.jsx
@@ -1,6 +1,8 @@
+import styles from "./Netflix.module.css"
+
 
 export const SeriesCards = (props) =>{
-    const {id,name, img_url,rating,description,genre,cast,watch_url} = props.data
+    const {name, img_url,rating,description,genre,cast,watch_url} = props.data
     // const {data} = props
     const btn={
       padding:"1.2rem 2.4rem",
@@ -11,17 +13,19 @@ export const SeriesCards = (props) =>{
       font:"blod",
       cursor:"Pointer"
     }
-  const ratingclass = rating >= 8.5? "super_hit": "average"
+  const ratingclass = rating >= 8.5? styles.super_hit: styles.average;
 
 
     return(
       
-        <li className="card">
+        <li className={styles.card}>
         <div>
           <img src={img_url} alt="abc.jpg" width= "30%" height="30%" />
-          <div className="card-content">
+
+          <div className={styles['card-content']}>
+
          <h2>Name:{name}</h2>  {/*  Dynamic Value using value */}
-         <h3>Rating:<span className={`rating ${ratingclass}`}>{rating}</span></h3> {/*  Dynamic Value using expression */}
+         <h3>Rating:<span className={`${styles.rating} ${ratingclass}`}>{rating}</span></h3> {/*  Dynamic Value using expression */}
          <p>{description}</p>  
          <p>Genre:{genre}</p> {/*  Dynamic Value using function */}
          <p>Cast:{cast}</p> {/*  Dynamic Value using function */}
