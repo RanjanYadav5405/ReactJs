2f48fe4e978147f259f448b7f0a275544873ded2
Ranjan Yadav <ranjanyadav5405@gmail.com>
2024-09-03 17:31:25 +0530

    styled-component

--- first-Website/src/components/Lists.jsx
+++ first-Website/src/components/Lists.jsx
@@ -1,18 +1,49 @@
 import styles from "./Netflix.module.css"
+import styled from "styled-components"
 
 
 export const SeriesCards = (props) =>{
     const {name, img_url,rating,description,genre,cast,watch_url} = props.data
     // const {data} = props
-    const btn={
-      padding:"1.2rem 2.4rem",
-      border:"none",
-      backgroundColor:rating >= 8.5?" #7dcea0" : "#f7dc6f" ,
-      color:"var(--btn-color)",
-      fontSize:"1.6rem",
-      font:"blod",
-      cursor:"Pointer"
-    }
+
+    // const btn={
+    //   padding:"1.2rem 2.4rem",
+    //   border:"none",
+    //   backgroundColor:rating >= 8.5?" #7dcea0" : "#f7dc6f" ,
+    //   color:"var(--btn-color)",
+    //   fontSize:"1.6rem",
+    //   font:"blod",
+    //   cursor:"Pointer"
+    // }
+
+
+    // const ButtonRanjan =styled.button({
+
+    //   padding:"1.2rem 2.4rem",
+    //   border:"none",
+    //   backgroundColor: ${rating >= 8.5?" #7dcea0" : "#f7dc6f"} ,
+    //   color:"var(--btn-color)",
+    //   fontSize:"1.6rem",
+    //   font:"blod",
+    //   cursor:"Pointer"
+    // })
+
+    const ButtonRanjan =styled.button`
+      padding:1.2rem 2.4rem;
+      border:none;
+      background-color: ${(props)=> props.rating >= 8.5? "#7dcea0" : "#f7dc6f"} ;
+      color:var(--btn-color);
+      font-size:1.6rem;
+      font:blod;
+      cursor:Pointer
+   `;
+
+   const Rating = styled.h3`
+          font-size: 1.6rem;
+          color: red;
+          text-transform:capitalize;
+   `;
+
   const ratingclass = rating >= 8.5? styles.super_hit: styles.average;
 
 
@@ -25,13 +56,13 @@ export const SeriesCards = (props) =>{
           <div className={styles['card-content']}>
 
          <h2>Name:{name}</h2>  {/*  Dynamic Value using value */}
-         <h3>Rating:<span className={`${styles.rating} ${ratingclass}`}>{rating}</span></h3> {/*  Dynamic Value using expression */}
+         <Rating>Rating:<span className={`${styles.rating} ${ratingclass}`}>{rating}</span></Rating> {/*  Dynamic Value using expression */}
          <p>{description}</p>  
          <p>Genre:{genre}</p> {/*  Dynamic Value using function */}
          <p>Cast:{cast}</p> {/*  Dynamic Value using function */}
          
           <a href={watch_url} target="_blank" >
-          <button style={btn}>Watch Now</button>
+          <ButtonRanjan rating= {rating}>Watch Now</ButtonRanjan>
           </a>
           </div>
         </div>
